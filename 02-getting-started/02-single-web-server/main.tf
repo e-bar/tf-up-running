4c2ac5a43c5057dcb5f191b2a557da3a90767fb1
e-bar <eric.br12@gmail.com>
2019-11-04 00:27:31 -0600

    creating an aws_security_group to allow the EC2 instance to receive traffic on port 8080

--- 02-getting-started/02-single-web-server/main.tf
+++ 02-getting-started/02-single-web-server/main.tf
@@ -6,6 +6,7 @@ provider "aws" {
 resource "aws_instance" "fusion" {
   ami           = "ami-0d5d9d301c853a04a"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -15,5 +16,16 @@ resource "aws_instance" "fusion" {
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
